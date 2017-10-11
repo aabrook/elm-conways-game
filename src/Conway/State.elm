ee70eb4ca65c5b98cb76187f29fd095755918282
Aaron Abrook <aabrook@gmail.com>
2017-10-11 21:41:54 +1000

    Update grid on tick

--- src/Conway/State.elm
+++ src/Conway/State.elm
@@ -10,7 +10,13 @@ init gen height width = ({ model | grid = prepareGrid gen height width }, Cmd.no
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Tick time -> ({ model | count = model.count + 1 }, Cmd.none)
+    Tick time -> (
+      { model
+      | count = model.count + 1
+      , grid = tickState model.grid
+      }
+      , Cmd.none
+    )
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
@@ -19,3 +25,17 @@ subscriptions model =
 prepareGrid : (Int -> Int -> Bool) -> Int -> Int -> List (List Bool)
 prepareGrid gen height width =
   List.map (\x -> (List.map (\y -> gen x y) <| List.range 0 width)) (List.range 0 height)
+
+tickState : Grid Bool -> Grid Bool
+tickState = List.map (\row -> List.map (\b -> isDead b 2) row)
+
+isDead : Bool -> Int -> Bool
+isDead dead neighbours = not dead
+{-
+  case neighbours of
+    0 -> False
+    1 -> False
+    2 -> dead
+    3 -> True
+    _ -> False
+-}
